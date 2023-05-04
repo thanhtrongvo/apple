96c85fc88d02d49265935a9c01c1cef7ebbe37c0
nKay83 <khaisx99@gmail.com>
2023-05-04 21:57:25 +0700

    add_to_cart

--- cartview.php
+++ cartview.php
@@ -25,13 +25,9 @@ include('php/cart.php');
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.4.js"></script>
-    <script src="js/ajax.js"></script>
 </head>
 
 <body>
-    <script >
-    </script> 
     <div class="app">
         <header class="header">
             <div class="grid">
@@ -80,9 +76,6 @@ include('php/cart.php');
                             </a>
                         </li>
                         <?php
-                        if (!isset($_SESSION)) {
-                            session_start();
-                        }
                         if (!isset($_SESSION['name'])) {
                             echo '<li class="header__navbar-item">
                             <a onclick="showModal()" class="header__navbar-login">
@@ -112,96 +105,14 @@ include('php/cart.php');
             </div>
             
         </header>
-        <div class="container">
-            <div class="container__content">
-                <div class="container__content-ip14--video">
-                    <video src="video/Ad_iPhone14pro.mp4" loop="" autoplay="" muted="" width="100%"></video>
-                </div>
-                <div class="container__content-ip14--btn">
-                    <button>
-                        BUY
-                    </button>
-                </div>
-            </div>
-
-            <div class="container__content-watch">
-                <div class="container__content-watch--photo">
-                    <img src="img/Apple-Watch-Ultra-Feature-Image.jpg" alt="">
-                </div>
-                <div class="container__content-watch--btn">
-                    <button>
-                        BUY
-                    </button>
-                </div>
-            </div>
-            <div class="container__content-last">
-                <div class="container__content-mac">
-                    <div class="container__content-mac--photo">
-                        <img src="img/AdMac.png" alt="">
-                    </div>
-                    <div class="container__content-mac--btn">
-                        <button>
-                            BUY
-                        </button>
-                    </div>
-                </div>
-                <div class="container__content-ipad">
-                    <div class="container__content-ipad--photo">
-                        <img src="img/AdIpad.png" alt="">
-                    </div>
-                    <div class="container__content-ipad--btn">
-                        <button>
-                            BUY
-                        </button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        <!-- Product -->
-        
-        <div class="product">
-            <div class="product__content">
-                <h1 class="product__content-heading">NEW IPHONE</h1>
-                <div class="product__content-iphone">
-                    <div class="home__product">
-                        <ul>
-                            <?php product_home("23");?>
-                        </ul>
-                    </div>
-                </div>
-                <h1 class="product__content-heading">NEW WATCH</h1>
-                <div class="product__content-watch">
-                    <div class="home__product">
-                        <ul>
-                            <?php product_home("24"); ?>
-                        </ul>
-                    </div>
-                </div>
-
-                <h1 class="product__content-heading">NEW IPAD</h1>
-                <div class="product__content-ipad">
-                    <div class="home__product">
-                        <ul>
-                            <?php product_home("18");?>
-                        </ul>
-                    </div>
-                </div>
-
-                <h1 class="product__content-heading">NEW MAC</h1>
-                <div class="product__content-mac">
-                    <div class="home__product">
-                        <ul>
-                            <?php product_home("19");?>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+<body >
+    <div style="height: 450px">
+        <?php
+            displayCart(); 
+        ?>
     </div>
-    <div id="footer">
+</body>
+<div id="footer">
         <div class="footer__content">
             <div class="footer__content--link">
                 <ul>
@@ -242,36 +153,3 @@ include('php/cart.php');
             </div>
         </div>
     </div>
-    <!-- modal -->
-    <div id="modal" class="modal">
-        <div class="modal__body">
-            <div class="auth-form">
-                <input type="checkbox" id="chk" aria-hidden="true">
-                <i onclick="exitModal()" class='fa-solid fa-xmark auth-form__btn--cancel'></i>
-                <div class="auth-form__signup">
-                    <form method="post">
-                        <label for="chk" aria-hidden="true">Join Us</label>
-                        <input type="text" name="name" placeholder="Full Name" required>
-                        <input type="text" name="phone" placeholder="Phone Number" required>
-                        <input type="email" name="email" placeholder="Email" required>
-                        <input type="password" name="pswd" placeholder="Password" required>
-                        <button type="submit" name="signup">Sign up</button>
-                    </form>
-                </div>
-                <div class="auth-form__login">
-                    <form method="post">
-                        <label for="chk" aria-hidden="true">Sign In</label>
-                        <input type="email" name="email" placeholder="Email" required>
-                        <input type="password" name="pswd" placeholder="Password" required>
-                        <button type="submit" name="signin">Sign In</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-<script src="js/login.js"> </script>
-<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-
-
-</html>
--- php/product.php
+++ php/product.php
@@ -4,20 +4,28 @@ function product_home($id){
     $syn = "SELECT * FROM `product` WHERE category_id = '$id'";
     $result = mysqli_query($conn,$syn);
         for($i=0; $i < 5; $i++ ){
-            $row = mysqli_fetch_row($result);
-            echo 
+            $row = mysqli_fetch_array($result);
+            
+            echo
             '<li class="home__product--info">
             <a href="">
                 <img src="'.$row[4].'" />
                 <h3>'.$row[2].'</h3>
                 <span class="price">'.number_format("$row[3]",0,".",".").'<u>đ</u></span>
+            </a>
+            <form class="form_item" method="post">
+                <input type="hidden" name="name" value="'.$row["title"].'">
+                <input type="hidden" name="id" value="'.$row["id"].'">
+                <input type="hidden" name="price" value="'.$row["price"].'">
+                <input type="hidden" name="image" value="'.$row["thumbnail"].'">
                 <div class="tooltip">
-                    <button class="themvaogio">
+                    <button class="themvaogio" type="submit" name="add_to_cart" value="addToCart">
                         <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
                         +
                     </button>
                 </div>
-            </a>
+            </form>
             </li>';
         }
 }
+?>
