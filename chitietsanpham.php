6c5717bb8db0d633bf9a9d4e2288267e16985096
Tran Khoi Nguyen <101859949+trankhoinguyen03@users.noreply.github.com>
2023-05-12 02:15:07 +0700

    Add files via upload

--- chitietsanpham.php
+++ chitietsanpham.php
@@ -1,9 +1,5 @@
-<?php 
-
-
-
+<?php
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,134 +8,131 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php 
-
-    ?> </title>
+    <title>CHI TIẾT SẢN PHẨM</title>
     <link rel="stylesheet" href="css/chitietsanpham.css">
-    <link rel="stylesheet" href="css/home_products.css">
-    <script src="js/chitietsanpham.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/base.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="/js/login.js"></script>
+
+    <link rel="shortcut icon" type="image/png" href="assets/img/logo.png" />
 </head>
 
 <body>
     <div class="main_container">
         <div class="container">
             <div class="left">
-                <div class="picture"><img src="assets/img/iphone13.png"></div>
+                <div class="picture"><img src="img/iphone13.png"></div>
             </div>
             <div class="right">
                 <h2>IPHONE 13</h2>
+                <div class="color">
+                    <h1>COLOR</h1>
+                    <div class="color_circlecheck">
+                        <input type="radio" id="a-color" class="circle-1" name="color_selector" checked>
+                        <label for="a-color">Green</label>
+
+                        <div class=" color_outer-circle">
+                        </div>
+                    </div>
+                    <div class="color_circlecheck">
+                        <input type="radio" id="b-color" class="circle-2" name="color_selector">
+                        <label for="b-color">Orange</label>
+
+                        <div class="color_outer-circle"></div>
+                    </div>
+                    <div class="color_circlecheck">
+                        <input type="radio" id="c-color" class="circle-3" name="color_selector">
+                        <label for="c-color">Purple</label>
+
+                        <div class="color_outer-circle"></div>
+                    </div>
+                </div>
+                <div class="option">
+                    <h1>OPTION</h1>
+                    <div class="option_circlecheck">
+                        <input type="radio" id="a-option" class="circle-4" name="option_selector" checked>
+                        <label for="a-option">128GB</label>
+
+                        <div class="option_outer-circle"></div>
+                    </div>
+                    <div class="option_circlecheck">
+                        <input type="radio" id="b-option" class="circle-5" name="option_selector">
+                        <label for="b-option">256GB</label>
+
+                        <div class="option_outer-circle"></div>
+                    </div>
+                    <div class="option_circlecheck">
+                        <input type="radio" id="c-option" class="circle-6" name="option_selector">
+                        <label for="c-option">512GB</label>
+
+                        <div class="option_outer-circle"></div>
+                    </div>
+                </div>
                 <span class="price">20.000.000<u>đ</u></span>
-                <span class="themgio"><button>THÊM VÀO GIỎ</button></span>
-                <div class="uudai">
-                    <span class="tieude">ƯU ĐÃI</span>
-                    <span>Chỉ từ 2k/ ngày có ngay Gói Bảo Hành 1 đổi 1 trong 1 năm</span>
-                    <span>Miễn phí Giao hàng siêu tốc trong 1 giờ</span>
-                    <span>Giảm thêm 30% tối đa 600.000 đ</span>
-                </div>
+                <span class="themgio"><button>ADD TO CART</button></span>
                 <div class="promo">
-                    <span>iPhone 13 128GB Chính Hãng (VN/A) hiện đang được bán tại Di Động Việt - Đại lý uỷ quyền chính
-                        thức của Apple tại Việt Nam. Với thiết kế thời thượng và các tính năng hiện đại, chiếc
-                        smartphone này sẽ mang đến cho bạn trải nghiệm đầy cảm xúc với khả năng sáng tạo vô tận từ 2
-                        camera 12MP, kết hợp với hiệu năng mạnh mẽ từ chipset Apple A15 Bionic sản xuất trên tiến trình
-                        5nm. Bên cạnh đó, với dung lượng pin lớn cho phép người dùng trải nghiệm dài lâu. Đặt mua iPhone
-                        13 tại Di Động Việt để có thể sở hữu chiếc máy sớm nhất với nhiều ưu đãi và khuyến mãi đi
-                        kèm.</span>
+                    <h3>Giới thiệu</h3>
+                    <span>iPhone 13 128GB - Đem đến những gì tuyệt vời nhất
+                        iPhone 13 128GB với thiết kế ngoại hình giống iPhone 12 đã quá “mãn nhãn” người dùng, lần này
+                        với
+                        những nâng cấp sâu hơn trên iPhone 13 về cấu hình cũng như camera, viên pin được nâng cấp từ
+                        dung
+                        lượng đến độ bền bỉ sẽ gây ấn tượng mạnh mẽ đến dàn iFans hùng hậu trên toàn thế giới.
+
+                        Giữ nguyên thiết kế vuông vức, sở hữu phần notch nhỏ hơn
+                        iPhone 13 128GB lần này cũng sẽ giữ nguyên vẹn vẻ ngoài của dòng iPhone 12 Series với thiết kế
+                        các
+                        cạnh vuông vức. Nhưng sẽ có vài điểm khác biệt dễ dàng có thể nhận ra, thứ nhất là về độ dày của
+                        thiết bị. Điện thoại iPhone 13 128GB sẽ có độ dày hơn khoảng 0,26mm vì được trang bị bên trong
+                        một
+                        viên pin với dung lượng lớn hơn.
+
+                        Giữ nguyên thiết kế vuông vức, sở hữu phần notch nhỏ hơn</span>
                 </div>
             </div>
         </div>
-        <div class="other">
-            <h3>SẢN PHẨM TƯƠNG TỰ</h3>
-            <div class="homeproduct">
-                <ul>
-                    <li class="sanPham">
-                        <a href="">
-                            <img src="assets/img/iphone13.png" />
-                            <h3>iphone13</h3>
-                            <span class="price">20.000.000<u>đ</u></span>
-                            <div class="tooltip">
-                                <button class="themvaogio">
-                                    <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
-                                    +
-                                </button>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="sanPham">
-                        <a href="">
-                            <img src="assets/img/iphone13.png" />
-                            <h3>iphone13</h3>
-                            <span class="price">20.000.000<u>đ</u></span>
-                            <div class="tooltip">
-                                <button class="themvaogio">
-                                    <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
-                                    +
-                                </button>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="sanPham">
-                        <a href="">
-                            <img src="assets/img/iphone13.png" />
-                            <h3>iphone13</h3>
-                            <span class="price">20.000.000<u>đ</u></span>
-                            <div class="tooltip">
-                                <button class="themvaogio">
-                                    <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
-                                    +
-                                </button>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="sanPham">
-                        <a href="">
-                            <img src="assets/img/iphone13.png" />
-                            <h3>iphone13</h3>
-                            <span class="price">20.000.000<u>đ</u></span>
-                            <div class="tooltip">
-                                <button class="themvaogio">
-                                    <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
-                                    +
-                                </button>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="bottom">
-            <div class="end_promo">
-                <span>iPhone 13 128GB - Đem đến những gì tuyệt vời nhất
-                    iPhone 13 128GB với thiết kế ngoại hình giống iPhone 12 đã quá “mãn nhãn” người dùng, lần này với
-                    những nâng cấp sâu hơn trên iPhone 13 về cấu hình cũng như camera, viên pin được nâng cấp từ dung
-                    lượng đến độ bền bỉ sẽ gây ấn tượng mạnh mẽ đến dàn iFans hùng hậu trên toàn thế giới.
+        <!-- end modal -->
 
-                    Giữ nguyên thiết kế vuông vức, sở hữu phần notch nhỏ hơn
-                    iPhone 13 128GB lần này cũng sẽ giữ nguyên vẹn vẻ ngoài của dòng iPhone 12 Series với thiết kế các
-                    cạnh vuông vức. Nhưng sẽ có vài điểm khác biệt dễ dàng có thể nhận ra, thứ nhất là về độ dày của
-                    thiết bị. Điện thoại iPhone 13 128GB sẽ có độ dày hơn khoảng 0,26mm vì được trang bị bên trong một
-                    viên pin với dung lượng lớn hơn.
-
-                    Giữ nguyên thiết kế vuông vức, sở hữu phần notch nhỏ hơn</span>
-            </div>
-            <div class="detail">
-                <h3>THÔNG SỐ KĨ THUẬT</h3>
-                <div class="ds_linh_kien">
-                    <span class="ten_linh_kien">Màn hình</span>
-                    <div class="chitiet">Chi tiết màn hình</div>
-                    <span class="ten_linh_kien">Camera sau</span>
-                    <div class="chitiet">Chi tiết camera sau</div>
-                    <span class="ten_linh_kien">Camera trước</span>
-                    <div class="chitiet">Chi tiết camera trước</div>
-                    <span class="ten_linh_kien">Hệ điều hành & CPU</span>
-                    <div class="chitiet">Chi tiết hệ điều hình & cpu</div>
-                    <span class="ten_linh_kien">Bộ nhớ & Lưu trữ</span>
-                    <div class="chitiet">Chi tiết bộ nhớ & lưu trữ</div>
-                    <span class="ten_linh_kien">Kết nối</span>
-                    <div class="chitiet">Chi tiết kết nối</div>
-                    <span class="ten_linh_kien">Pin & Sạc</span>
-                    <div class="chitiet">Chi tiết pin & sạc</div>
-                    <span class="ten_linh_kien">Thiết kế</span>
-                    <div class="chitiet">Chi tiết thiết kế</div>
+        <div id="footer">
+            <div class="footer__content">
+                <div class="footer__content--link">
+                    <ul>
+                        <h2>Product</h2>
+                        <li><a href="">MAC</a></li>
+                        <li><a href="">IPAD</a></li>
+                        <li><a href="">IPHONE</a></li>
+                        <li><a href="">WATCH</a></li>
+                        <li><a href="">AIRPOD</a></li>
+                    </ul>
+                </div>
+                <div class="footer__content--link">
+                    <ul>
+                        <h2>About Apple</h2>
+                        <li><a href="">Newsroom</a></li>
+                        <li><a href="">Career Opportunities</a> </li>
+                        <li><a href="">Investors</a></li>
+                        <li><a href="">Contact Apple </a></li>
+                    </ul>
+                </div>
+                <div class="footer__content--link">
+                    <ul>
+                        <h2>Social</h2>
+                        <li><a href=""><i class='bx bxl-facebook-circle'></i> Facebook</a></li>
+                        <li><a href=""><i class='bx bxl-instagram-alt'></i> Instagram</a> </li>
+                        <li><a href=""><i class='bx bxl-youtube'></i> Youtube</a> </li>
+                        <li><a href=""><i class='bx bxl-twitter'></i> Twitter</a></li>
+                    </ul>
+                </div>
+                <div class="footer__content--link">
+                    <ul>
+                        <h2>Create By</h2>
+                        <li><a href="">Thanh Phong</a> </li>
+                        <li><a href="">Thanh Trọng</a> </li>
+                        <li><a href="">Nhật Khải</a> </li>
+                        <li><a href="">Khôi Nguyên</a> </li>
+                    </ul>
                 </div>
             </div>
         </div>
