ebe078336575f6a0d5076ed9cf83b121dcb9a3ac
Tran Khoi Nguyen <101859949+trankhoinguyen03@users.noreply.github.com>
2023-05-12 15:33:19 +0700

    Add files via upload

--- php/product.php
+++ php/product.php
@@ -1,37 +1,24 @@
-<?php 
-function product_all(){
-    include('database/connection.php');
-    $syn = "SELECT * FROM `product`";
-    $result = mysqli_query($conn,$syn);
-        for($i=0; $i < 12; $i++ ){
-            $row = mysqli_fetch_array($result);
-            innerProduct($row['thumbnail'],$row['title'],$row['price'],$row['id']);
-            
-        }
-}
-function product_home($id){
+<?php
+function product_home($id)
+{
     include('database/connection.php');
     $syn = "SELECT * FROM `product` WHERE category_id = '$id'";
-    $result = mysqli_query($conn,$syn);
-        for($i=0; $i < 5; $i++ ){
-            $row = mysqli_fetch_array($result);
-            innerProduct($row['thumbnail'],$row['title'],$row['price'],$row['id']);
-            
-        }
-}
-function innerProduct($img, $title, $price, $id){
-    echo
+    $result = mysqli_query($conn, $syn);
+    for ($i = 0; $i < 4; $i++) {
+        $row = mysqli_fetch_array($result);
+
+        echo
             '<li class="home__product--info">
             <a href="">
-                <img src="'.$img.'" />
-                <h3>'.$title.'</h3>
-                <span class="price">'.number_format("$price",0,".",".").'<u>đ</u></span>
+                <img src="' . $row[4] . '" />
+                <h3>' . $row[2] . '</h3>
+                <span class="price">' . number_format("$row[3]", 0, ".", ".") . '<u>đ</u></span>
             </a>
             <form class="form_item" method="post">
-                <input type="hidden" name="name" value="'.$title.'">
-                <input type="hidden" name="id" value="'.$id.'">
-                <input type="hidden" name="price" value="'.$price.'">
-                <input type="hidden" name="image" value="'.$img.'">
+                <input type="hidden" name="name" value="' . $row["title"] . '">
+                <input type="hidden" name="id" value="' . $row["id"] . '">
+                <input type="hidden" name="price" value="' . $row["price"] . '">
+                <input type="hidden" name="image" value="' . $row["thumbnail"] . '">
                 <div class="tooltip">
                     <button class="themvaogio" type="submit" name="add_to_cart" value="addToCart">
                         <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
@@ -40,13 +27,39 @@ function innerProduct($img, $title, $price, $id){
                 </div>
             </form>
             </li>';
+    }
 }
-function select_category() {
+
+function select_category()
+{
     include('database/connection.php');
-    if(isset($_GET['category'])){
+    if (isset($_GET['category'])) {
         $cate = $_GET['category'];
-    product_home($cate);
-}
+        $sql = "SELECT * FROM `product` WHERE category_id = '$cate'";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+            echo
+                '<li class="home__product--info">
+            <a href="">
+                <img src="' . $row[4] . '" />
+                <h3>' . $row[2] . '</h3>
+                <span class="price">' . number_format("$row[3]", 0, ".", ".") . '<u>đ</u></span>
+            </a>
+            <form class="form_item" method="post">
+                <input type="hidden" name="name" value="' . $row["title"] . '">
+                <input type="hidden" name="id" value="' . $row["id"] . '">
+                <input type="hidden" name="price" value="' . $row["price"] . '">
+                <input type="hidden" name="image" value="' . $row["thumbnail"] . '">
+                <div class="tooltip">
+                    <button class="themvaogio" type="submit" name="add_to_cart" value="addToCart">
+                        <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
+                        +
+                    </button>
+                </div>
+            </form>
+            </li>';
+        }
+    }
 }
 
 ?>
