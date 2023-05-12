9f2db6d63aefdeb78432a569c93fab171c7c917d
nKay83 <khaisx99@gmail.com>
2023-05-12 14:52:39 +0700

    up

--- php/product.php
+++ php/product.php
@@ -1,23 +1,37 @@
 <?php 
+function product_all(){
+    include('database/connection.php');
+    $syn = "SELECT * FROM `product`";
+    $result = mysqli_query($conn,$syn);
+        for($i=0; $i < 12; $i++ ){
+            $row = mysqli_fetch_array($result);
+            innerProduct($row['thumbnail'],$row['title'],$row['price'],$row['id']);
+            
+        }
+}
 function product_home($id){
     include('database/connection.php');
     $syn = "SELECT * FROM `product` WHERE category_id = '$id'";
     $result = mysqli_query($conn,$syn);
         for($i=0; $i < 5; $i++ ){
             $row = mysqli_fetch_array($result);
+            innerProduct($row['thumbnail'],$row['title'],$row['price'],$row['id']);
             
-            echo
+        }
+}
+function innerProduct($img, $title, $price, $id){
+    echo
             '<li class="home__product--info">
             <a href="">
-                <img src="'.$row[4].'" />
-                <h3>'.$row[2].'</h3>
-                <span class="price">'.number_format("$row[3]",0,".",".").'<u>đ</u></span>
+                <img src="'.$img.'" />
+                <h3>'.$title.'</h3>
+                <span class="price">'.number_format("$price",0,".",".").'<u>đ</u></span>
             </a>
             <form class="form_item" method="post">
-                <input type="hidden" name="name" value="'.$row["title"].'">
-                <input type="hidden" name="id" value="'.$row["id"].'">
-                <input type="hidden" name="price" value="'.$row["price"].'">
-                <input type="hidden" name="image" value="'.$row["thumbnail"].'">
+                <input type="hidden" name="name" value="'.$title.'">
+                <input type="hidden" name="id" value="'.$id.'">
+                <input type="hidden" name="price" value="'.$price.'">
+                <input type="hidden" name="image" value="'.$img.'">
                 <div class="tooltip">
                     <button class="themvaogio" type="submit" name="add_to_cart" value="addToCart">
                         <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
@@ -26,37 +40,12 @@ function product_home($id){
                 </div>
             </form>
             </li>';
-        }
 }
-
 function select_category() {
     include('database/connection.php');
     if(isset($_GET['category'])){
         $cate = $_GET['category'];
-        $sql = "SELECT * FROM `product` WHERE category_id = '$cate'";
-        $result = mysqli_query($conn,$sql);
-        while($row = mysqli_fetch_array($result)){
-            echo
-            '<li class="home__product--info">
-            <a href="">
-                <img src="'.$row[4].'" />
-                <h3>'.$row[2].'</h3>
-                <span class="price">'.number_format("$row[3]",0,".",".").'<u>đ</u></span>
-            </a>
-            <form class="form_item" method="post">
-                <input type="hidden" name="name" value="'.$row["title"].'">
-                <input type="hidden" name="id" value="'.$row["id"].'">
-                <input type="hidden" name="price" value="'.$row["price"].'">
-                <input type="hidden" name="image" value="'.$row["thumbnail"].'">
-                <div class="tooltip">
-                    <button class="themvaogio" type="submit" name="add_to_cart" value="addToCart">
-                        <span class="tooltiptext" style="font-size: 15px;">Thêm vào giỏ</span>
-                        +
-                    </button>
-                </div>
-            </form>
-            </li>';
-        }
+    product_home($cate);
 }
 }
 
