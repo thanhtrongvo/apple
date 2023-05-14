98eca669a0723dfc11060539fb6293f59f1b20b4
Thanh Trong Vo <votrong1471@gmail.com>
2023-05-14 20:57:08 +0700

    up

--- admin/index.php
+++ admin/index.php
@@ -21,6 +21,7 @@ if (!($_SESSION['role'])) {
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../plugins/canvasjs-3.7.5/canvasjs.angular.component.ts">
   <style>
     .chart-container {
       position: relative;
@@ -80,7 +81,7 @@ if (!($_SESSION['role'])) {
         </div>
 
         <div class="chart-container">
-          <canvas id="chart-order"></canvas>
+          <div id="chartContainer" style="height: 300px; width: 100%;"></div>
 
         </div>
       </section>
@@ -102,6 +103,11 @@ if (!($_SESSION['role'])) {
   <!-- ./wrapper -->
 
   <!-- jQuery -->
+  <script src="../plugins/canvasjs-3.7.5/canvasjs.min.js"></script>
+  <script src="../plugins/canvasjs-3.7.5/jquery.canvasjs.min.js"></script>
+
+  <script src="../plugins/chart.js/Chart.js"></script>
+  <script src="../plugins/jquery/jquery.js"></script>
   <script src="../plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
   <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -182,8 +188,64 @@ if (!($_SESSION['role'])) {
     //   });
     // });
   </script>
-  <script src="../plugins/chart.js/Chart.js"></script>
-  <script src="../plugins/jquery/jquery.js"></script>
+  <script>
+    window.onload = function() {
+
+      var options = {
+        title: {
+          text: "Desktop OS Market Share in 2017"
+        },
+        subtitles: [{
+          text: "As of November, 2017"
+        }],
+        animationEnabled: true,
+        data: [{
+          type: "pie",
+          startAngle: 40,
+          toolTipContent: "<b>{label}</b>: {y}%",
+          showInLegend: "true",
+          legendText: "{label}",
+          indexLabelFontSize: 16,
+          indexLabel: "{label} - {y}%",
+          dataPoints: [{
+              y: 48.36,
+              label: "Windows 7"
+            },
+            {
+              y: 26.85,
+              label: "Windows 10"
+            },
+            {
+              y: 1.49,
+              label: "Windows 8"
+            },
+            {
+              y: 6.98,
+              label: "Windows XP"
+            },
+            {
+              y: 6.53,
+              label: "Windows 8.1"
+            },
+            {
+              y: 2.45,
+              label: "Linux"
+            },
+            {
+              y: 3.32,
+              label: "Mac OS X 10.12"
+            },
+            {
+              y: 4.03,
+              label: "Others"
+            }
+          ]
+        }]
+      };
+      $("#chartContainer").CanvasJSChart(options);
+
+    }
+  </script>
 
 
 </body>
